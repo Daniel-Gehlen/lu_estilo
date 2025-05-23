97ef311451544c39dd744b471452db338fbc3a34
Daniel <danielgehlentech@gmail.com>
2025-05-22 23:21:15 -0300

    build: configura containerização com Python 3.9

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker-compose build
-docker-compose up -d
-sleep 5  # Espera o banco iniciar
-docker-compose exec web alembic revision --autogenerate -m "init"
-docker-compose exec web alembic upgrade head
-echo "✅ API PRONTA em http://localhost:8000"
-docker-compose logs web -f
